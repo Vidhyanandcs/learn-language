9cd556276849b553ed8997298184c4288239594d
Vidhyanandcs <vithushaji@gmail.com>
2021-05-28 10:58:19 +0530

    updated exercise

--- public/js/listen.js
+++ public/js/listen.js
@@ -1,16 +1,27 @@
 let matching = document.getElementById('matching')
 let notmatching = document.getElementById('not-matching')
+let showAnswer = document.getElementById('showAnswer')
+let answer = document.getElementById('answer1')
 
 //Listen Listen
+if(matching){
+	matching.addEventListener("click", function(){
+		matching.style.backgroundColor = "#4aa96c"
+		notmatching.disabled = true
+		console.log("matching")
+	})
+}
 
-matching.addEventListener("click", function(){
-	matching.style.backgroundColor = "#4aa96c"
-    notmatching.disabled = true
-	console.log("matching")
-})
+if(notmatching){
+	notmatching.addEventListener("click", function(){
+		notmatching.style.backgroundColor = "#f55c47"
+		matching.disabled = true
+	})
+}
 
-notmatching.addEventListener("click", function(){
-	notmatching.style.backgroundColor = "#f55c47"
-    matching.disabled = true
-	console.log("matching")
-})
+if(showAnswer){
+	showAnswer.addEventListener("click", function(){
+		answer.style.backgroundColor = "#4aa96c"
+		answer.style.color = "#ffffff"
+	})
+}
